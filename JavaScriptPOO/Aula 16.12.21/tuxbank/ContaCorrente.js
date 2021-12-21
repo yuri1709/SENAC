87a4906a961c917130740d2a747656809a3b94fa
yuri1709 <yuriconder184@gmail.com>
2021-12-20 23:57:50 -0300

    class composition has not added yet

--- JavaScriptPOO/Aula 16.12.21/tuxbank/ContaCorrente.js
+++ JavaScriptPOO/Aula 16.12.21/tuxbank/ContaCorrente.js
@@ -1,6 +1,8 @@
 class ContaCorrente {
+   Vdeposito;
    
     constructor(){
+       
         this.encapSaldo =0;
         this.agencia = prompt("Número agencia");
         this.operacao = prompt("Sacar, Depositar ou Transferir: ");
@@ -13,12 +15,14 @@ class ContaCorrente {
         }else if(this.operacao == 'depositar'){
             let deposito = prompt("Informe o valor do deposito: ");
             this.depositar(deposito);
+            this.Vdeposito = deposito;
         }else if (this.operacao == 'transferir'){
             let transfere = prompt("Informe o valor de trânsferencia: ")
             this.trasnferir(transfere) ;
         }else {
             alert("Operaçao não realizada!");
         }
+       
     }
     //Somente para informar o valor do saldo.
     get encapSaldo(){
@@ -28,12 +32,13 @@ class ContaCorrente {
     set encapSaldo(valor){
         this._saldo = parseInt(valor);
     }
+    //Parei aqui,favor continuar
     
     depositar(deposito){
-        this._saldo += parseInt(deposito); //Corrigido o erro do deposito.
-        return deposito;
-    }
-
+        this._saldo += parseFloat(deposito); //Corrigido o erro do deposito.
+     }
+   
+    
     sacar(saque){
         if (this._saldo > 0) {
             this._saldo -= saque;
@@ -52,5 +57,5 @@ class ContaCorrente {
             alert("Saldo Insuficiente");
         }
     }
-
+ 
 }
--- JavaScriptPOO/Aula 16.12.21/tuxbank/script.js
+++ JavaScriptPOO/Aula 16.12.21/tuxbank/script.js
@@ -4,4 +4,6 @@ document.write(`CPF:${Clientes.encapCpf}<br>`);
 
 const ContasC = new ContaCorrente();
 document.write("Saldo", ContasC.encapSaldo+ "<br>");
-
+if(ContasC.Vdeposito != undefined){ 
+document.write("Valor depositado",ContasC.Vdeposito);
+};

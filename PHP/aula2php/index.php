471519c517374b4da45ed54c56e3136c00f1aba4
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-02-04 11:28:41 -0300

    ajuda no dever de casa

---
+++ PHP/aula2php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bem vindo a página inicial</h2>
+    <div class="container">
+        <a href="cadastroCliente.php">Cadastro Cliente</a>
+        <a href="consultaCliente.php">Consulta Cliente</a>
+        <a href="cadastroAluno.php">Cadastro Aluno</a>
+    </div>
+</body>
+</html>

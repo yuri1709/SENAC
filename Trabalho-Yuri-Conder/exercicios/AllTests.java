74d825f0299a75a05bd199f31ebb3e6bdf73a2e2
yuri1709 <81831168+yuri1709@users.noreply.github.com>
2022-05-18 20:10:19 -0300

    Organizando o código.

--- Trabalho-Yuri-Conder/exercicios/AllTests.java
+++ Trabalho-Yuri-Conder/exercicios/AllTests.java
@@ -8,10 +8,10 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 // Reunindo todos os testes em uma única classe
 @SuiteClasses({ AllTests.class, 
-				DivisaoInteiraTeste.class,
-				MultiplicacaoTeste.class,
-				SomaTeste.class,
-				SubtracaoTeste.class
+		DivisaoInteiraTeste.class,
+		MultiplicacaoTeste.class,
+		SomaTeste.class,
+		SubtracaoTeste.class
 })
 
 
